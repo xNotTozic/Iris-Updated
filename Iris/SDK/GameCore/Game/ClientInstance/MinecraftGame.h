15d939e123aed1a49b2cdd6f03a5b307d0e6d3f5
Tozic Gaming <isameldein2012@gmail.com>
2024-02-13 20:33:35 +0400

    canusemovekeys

--- Iris/SDK/GameCore/Game/ClientInstance/MinecraftGame.h
+++ Iris/SDK/GameCore/Game/ClientInstance/MinecraftGame.h
@@ -5,7 +5,8 @@ class MinecraftGame
 public:
 	bool canUseMoveKeys()
 	{
-		static unsigned int offset = 0x108; // offset sig here
+		static unsigned int offset = 0x190; // offset sig here // Updated to 1.20.51
+		//static unsigned int offset = 0x108; // 1.20.0.1
 		return *reinterpret_cast<bool*>((uintptr_t)this + offset);
 	}
 };
