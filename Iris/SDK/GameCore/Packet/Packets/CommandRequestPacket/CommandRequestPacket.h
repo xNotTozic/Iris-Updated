60760ac0fa2c18ffb007742783abb731fdd2da0f
Tozic Gaming <isameldein2012@gmail.com>
2024-02-13 20:36:06 +0400

    CommandRequstPacket 1.20.51

--- Iris/SDK/GameCore/Packet/Packets/CommandRequestPacket/CommandRequestPacket.h
+++ Iris/SDK/GameCore/Packet/Packets/CommandRequestPacket/CommandRequestPacket.h
@@ -6,7 +6,7 @@
 class CommandRequestPacket : public Packet
 {
 public:
-    enum class Origin 
+    enum class Origin
     {
         PLAYER = 0,
         COMMAND_BLOCK = 1,
@@ -22,7 +22,7 @@ public:
     };
 
     CommandRequestPacket() { init(); };
-    CommandRequestPacket(std::string command, Origin origin = Origin::PLAYER, bool isExternal = 0) 
+    CommandRequestPacket(std::string command, Origin origin = Origin::PLAYER, bool isExternal = 0)
     {
         init();
         this->command = command;
@@ -31,14 +31,16 @@ public:
     }
 
     TextHolder command;
-	Origin origin = Origin::PLAYER;
-	bool isExternal = 0;
-	char pad[0x256];
+    Origin origin = Origin::PLAYER;
+    bool isExternal = 0;
+    char pad[0x256];
 
     void init()
     {
         static void* sig = nullptr;
-        if (!sig) sig = findSig("48 8D 05 ? ? ? ? 48 8B F9 48 89 01 8B DA 48 83 C1 68 E8 ? ? ? ? 48 8D 4F 30 E8 ? ? ? ? 48 8D 05 ? ? ? ?");
+        if (!sig) sig = findSig("48 8D 05 ? ? ? ? 48 89 73 28 48 89"); // Updated to 1.20.51
+        // 48 8D 05 ? ? ? ? 48 89 73 28 48 89 43 10 C7 43 ? ? ? ? ? C7 43 ? ? ? ? ? 66 89 73 20 48 89 73 30 89 73 38 0F 11 43 40 48 89 73 50 48 C7 43 ? ? ? ? ?
+        // 48 8D 05 ? ? ? ? 48 8B F9 48 89 01 8B DA 48 83 C1 68 E8 ? ? ? ? 48 8D 4F 30 E8 ? ? ? ? 48 8D 05 ? ? ? ? in 1.20.0.1
         this->setVTable<CommandRequestPacket>(reinterpret_cast<uintptr_t*>(sig));
     }
 };
