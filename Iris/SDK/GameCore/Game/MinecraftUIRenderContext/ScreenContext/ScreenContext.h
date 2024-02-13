e8ed2eb44891722a896dc3ed2206d0bcf5a84fbe
Tozic Gaming <isameldein2012@gmail.com>
2024-02-13 20:34:53 +0400

    tessellator offset

--- Iris/SDK/GameCore/Game/MinecraftUIRenderContext/ScreenContext/ScreenContext.h
+++ Iris/SDK/GameCore/Game/MinecraftUIRenderContext/ScreenContext/ScreenContext.h
@@ -9,6 +9,7 @@ public:
     }
 
     inline Tessellator* getTessellator() {
-        return *reinterpret_cast<Tessellator**>((uintptr_t)(this) + 0xC0);
+        return *reinterpret_cast<Tessellator**>((uintptr_t)(this) + 0xC0); // Updated to 1.20.51
+        // The same offset in 1.20.0.1
     }
 };
