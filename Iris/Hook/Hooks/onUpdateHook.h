765777ac02cfaa47447c06f59273b320c6009cef
Tozic Gaming <isameldein2012@gmail.com>
2024-03-16 20:38:20 +0400

    Well this commit is a unique one from all the others this signature is like the motherboard of the CPU it is the main thing to make all the modules work so we have established a great job well done by doing that this signature's function or name is unknown so we don't know how to find it but for this one here we are using TickAndRenderUI We really wish it works best of luck for all of you people who trying to copy it and to those fellow toddlers whom doesn't know a thing on how to reverse or how to read English a summary of this sentence is: "bruv i dont knoww if this will work or nah :sob:"

--- Iris/Hook/Hooks/onUpdateHook.h
+++ Iris/Hook/Hooks/onUpdateHook.h
@@ -26,7 +26,9 @@ public:
 
     bool Initialize() override
     {
-        void* updateTick = findSig(xorstr_("48 8b c4 48 89 58 ? 48 89 70 ? 48 89 78 ? 55 41 54 41 55 41 56 41 57 48 8d a8 ? ? ? ? 48 81 ec ? ? ? ? 0f 29 70 ? 48 8b 05 ? ? ? ? 48 33 c4 48 89 85 ? ? ? ? 4c 8b f9 45 33 ed"));
+        void* updateTick = findSig(xorstr_("48 8B C4 48 89 58 18 55 56 57 41 54 41 55 41 56 41 57 48 8D A8 ? ? ? ? 48 81 EC ? ? ? ? 0F 29 70 B8 0F 29 78 A8 48 8B 05 ? ? ? ? 48 33 C4 48 89 85 ? ? ? ?"));
+        // 48 8b c4 48 89 58 ? 48 89 70 ? 48 89 78 ? 55 41 54 41 55 41 56 41 57 48 8d a8 ? ? ? ? 48 81 ec ? ? ? ? 0f 29 70 ? 48 8b 05 ? ? ? ? 48 33 c4 48 89 85 ? ? ? ? 4c 8b f9 45 33 ed
+        // 48 8B C4 48 89 58 18 55 56 57 41 54 41 55 41 56 41 57 48 8D A8 ? ? ? ? 48 81 EC ? ? ? ? 0F 29 70 B8 0F 29 78 A8 48 8B 05 ? ? ? ? 48 33 C4 48 89 85 ? ? ? ? 4C 8B FA 
         return HookFunction(updateTick, (void*)&onUpdate, &oUpdateTick, xorstr_("Tick"));
     }
 };
